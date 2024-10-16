0886300e2238db8acaa5c9f9c80e1520ecdc946b
Maksym Galchynskyi <mgalchinskyi@gmail.com>
2024-10-16 12:18:13 +0200

    feat: Add token mapping class

---
+++ EasyjobTestSolution.WebApi/Models/TokenResponse.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EasyjobTestSolution.WebApi.Models
+{
+    public sealed class TokenResponse
+    {
+        [JsonPropertyName("access_token")]
+        public string AccessToken { get; set; }= string.Empty;
+
+        [JsonPropertyName("token_type")]
+        public string TokenType { get; set; } = string.Empty;
+
+        [JsonPropertyName("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonPropertyName("userName")]
+        public string UserName { get; set; } = string.Empty;
+
+        [JsonPropertyName(".issued")]
+        public string Issued { get; set; } = string.Empty;
+
+        [JsonPropertyName(".expires")]
+        public string Expires { get; set; } = string.Empty;
+    }
+}
