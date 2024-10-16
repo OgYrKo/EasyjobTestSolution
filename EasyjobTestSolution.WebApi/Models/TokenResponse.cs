8f24064182abd5f506b577d8ca161008b42793b1
Maksym Galchynskyi <mgalchinskyi@gmail.com>
2024-10-16 17:02:15 +0200

    feat: Update TokenResponse class structure

--- EasyjobTestSolution.WebApi/Models/TokenResponse.cs
+++ EasyjobTestSolution.WebApi/Models/TokenResponse.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace EasyjobTestSolution.WebApi.Models
 {
@@ -17,9 +18,39 @@ namespace EasyjobTestSolution.WebApi.Models
         public string UserName { get; set; } = string.Empty;
 
         [JsonPropertyName(".issued")]
-        public string Issued { get; set; } = string.Empty;
+        public string IssuedString 
+        {
+            get
+            {
+                return Issued.ToString(DateFormat);
+            }
+            set
+            {
+                Issued = DateTime.ParseExact(value, DateFormat, CultureInfo.InvariantCulture);
+            }
+        } 
+
+        public DateTime Issued { get; set; }
 
         [JsonPropertyName(".expires")]
-        public string Expires { get; set; } = string.Empty;
+        public string ExpiresString
+        {
+            get
+            {
+                return Expires.ToString(DateFormat);
+            }
+            set
+            {
+                Expires = DateTime.ParseExact(value, DateFormat, CultureInfo.InvariantCulture);
+            }
+        }
+        public DateTime Expires { get; set; }
+
+        private const string DateFormat = "ddd, dd MMM yyyy HH:mm:ss 'GMT'";
+
+        public bool IsValid()
+        {
+            return !string.IsNullOrEmpty(AccessToken) && DateTime.UtcNow < Expires;
+        }
     }
 }
