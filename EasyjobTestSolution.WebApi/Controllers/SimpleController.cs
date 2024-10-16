0771c0c54502d90d86bcd0c462c383254f566c45
Maksym Galchynskyi <mgalchinskyi@gmail.com>
2024-10-16 10:57:26 +0200

    feat: Init project

---
+++ EasyjobTestSolution.WebApi/Controllers/SimpleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyjobTestSolution.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SimpleController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
